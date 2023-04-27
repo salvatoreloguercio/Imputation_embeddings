fba33da846d1c8279e46e99e9929432c6bc9b79b
salvatoreloguercio <salvatore.loguercio@gmail.com>
2023-04-26 17:51:31 -0700

    minor fixes

--- lift_hm.sh
+++ lift_hm.sh
@@ -2,7 +2,7 @@
 
 while read path_line; do
 
- vcf=$(basename $path_line .vcf.gz)
+ vcf=$(basename $path_line .vcf.gz | cut -d '.' -f3,5)
 
   sbatch --export=input=$path_line --job-name=lift_$vcf --output=$vcf.out --cpus-per-task=16 --mem=90G --time=40:00:00 picard_lift_2.25.sbatch;
   sleep 1;
